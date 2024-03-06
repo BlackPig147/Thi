c98a42cacb9b2f262dd64af18bef1ccdb80ebaad
BlackPig147 <nhut014@gmail.com>
2024-03-06 15:03:17 +0700

    5

--- login.php
+++ login.php
@@ -2,6 +2,6 @@
 session_start();
 ?>
 <form action = "login.php" method="post">
-    Username:<input type = "text" name="txtUserName"><br>
-    Mật khẩu: <input type="password" name="txtmatkhau"><br>
+    tên đăng nhập:<input type = "text" name="txtUserName"><br>
+    mật khẩu: <input type="password" name="txtmatkhau"><br>
 </form>
